7079be6ef9f8ce2e6d23ae2512f0da80f30685e5
Pablo Gallego Carmona <pablogallegocarmona@gmail.com>
2022-04-06 20:15:04 +0200

    fix firebase & helmet

--- src/pages/index.js
+++ src/pages/index.js
@@ -51,6 +51,33 @@ const IndexPage = () => {
                         href={favicon}
                         sizes="16x16"
                     />
+
+                    <meta name="twitter:card" content="summary_large_image" />
+                    <meta name="twitter:site" content="@TU_USER" />
+                    <meta name="twitter:creator" content="@TU_USER" />
+                    <meta name="twitter:title" content="Portafolio Pablo" />
+                    <meta
+                        name="twitter:description"
+                        content="Portafolio Pablo"
+                    />
+                    <meta
+                        name="twitter:image"
+                        content="https://s3.amazonaws.com/gndx.dev/gndxdev.png"
+                    />
+                    <meta property="og:title" content="Portafolio Pablo" />
+                    <meta
+                        property="og:description"
+                        content="Portafolio Pablo"
+                    />
+                    <meta
+                        property="og:image"
+                        content="https://s3.amazonaws.com/gndx.dev/gndxdev.png"
+                    />
+                    <meta property="og:url" content="pablogallegocarmona.xyz" />
+                    <meta property="og:site_name" content="Portafolio Pablo" />
+                    <meta property="og:locale" content="es_ES" />
+                    <meta property="og:type" content="article" />
+                    <meta property="fb:app_id" content="ID_APP_FACEBOOK" />
                 </Helmet>
                 <Layout>
                     {/* <Navbar isScrolling={scrollHeight} /> */}
