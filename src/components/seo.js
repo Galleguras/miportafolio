7f529e01a10d3effd0416d75b8dce5257bf16ef8
pgallego <pablo.gallego-carmona@t-systems.com>
2023-03-21 17:23:48 +0100

    migrate gatsby-plugin-react-helmet to API Gatsby Head

--- src/components/seo.js
+++ src/components/seo.js
@@ -4,16 +4,25 @@ import MiFoto from '../media/yo.png'
 import favicon from '../media/imgPestaña.ico'
 import { useTranslation } from 'gatsby-plugin-react-i18next'
 
-export const SEO = ({ children }) => {
+export const SEO = ({
+    title,
+    description,
+    autor,
+    image,
+    siteUrl,
+    pathname,
+    children
+}) => {
     const { t } = useTranslation()
-    const { title, description, author, image, siteUrl } = useSiteMetadata()
+    const { title: defaultTitle, description: defaultDescription } =
+        useSiteMetadata()
 
     const seo = {
-        title: title,
-        description: description,
+        title: title || defaultTitle,
+        description: description || defaultDescription,
         image: `${siteUrl}${image}`,
-        url: `${siteUrl}`,
-        autor: `${author}`
+        url: `${siteUrl}${pathname || ``}`,
+        autor
     }
 
     return (
@@ -21,7 +30,6 @@ export const SEO = ({ children }) => {
             <title>{seo.title}</title>
             <link rel="icon" type="image/png" href={favicon} sizes="16x16" />
             <meta name="description" content={seo.description} />
-
             <meta name="twitter:site" content="@pablogallegoca2" />
             <meta name="twitter:creator" content="@pablogallegoca2" />
             <meta name="twitter:title" content={seo.title} />
@@ -34,7 +42,7 @@ export const SEO = ({ children }) => {
             <meta property="og:image" itemprop="image" content={MiFoto} />
             <meta property="og:image:width" content="30" />
             <meta property="og:image:height" content="30" />
-            <meta property="og:site_name" content={t('Portafolo Pablo')} />
+            <meta property="og:site_name" content={seo.title} />
             <meta property="og:locale" content="es_ES" />
             <meta property="og:type" content="article" />
             {children}
